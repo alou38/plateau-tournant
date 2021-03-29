2c1fed8964c4c07f1ad206995e813d02e654e1b6
alou38 <72703710+alou38@users.noreply.github.com>
2021-03-29 12:53:24 +0200

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-def _1_degret():
-    pass
-
 def on_button_pressed_a():
-    global NbPhoto, CTRlimite
+    global NbPhoto, compteur
     basic.pause(100)
     NbPhoto += 1
     basic.pause(100)
-    CTRlimite = NbPhoto
+    compteur = NbPhoto
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def winner():
+    basic.clear_screen()
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        """)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    basic.show_icon(IconNames.DIAMOND)
+    basic.show_icon(IconNames.DIAMOND)
+    basic.pause(2000)
+    basic.clear_screen()
+
 def on_button_pressed_b():
-    global NbPhoto, CTRlimite
+    global NbPhoto, compteur
     basic.pause(100)
     NbPhoto += -1
     basic.pause(100)
-    CTRlimite = NbPhoto
+    compteur = NbPhoto
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_pin_pressed_p1():
     global Calculateur, CTRlimite, compteur
     # calcule
-    Calculateur = 3.8 + NbPhoto
+    Calculateur = RATIO / NbPhoto
     CTRlimite = Calculateur
     compteur = NbPhoto
     # calcule
-    while CTRlimite < 3.8:
+    while CTRlimite < RATIO:
         for index in range(NbPhoto):
             for index2 in range(rotation):
                 _1_degret()
@@ -36,30 +48,47 @@ def on_pin_pressed_p1():
 input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
 
 def PriseDeVue():
-    pass
-compteur = 0
-Calculateur = 0
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    basic.pause(1000)
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    basic.pause(1000)
+def _1_degret():
+    motor.set_delay(10)
+    motor.move_clockwise(Calculateur, stepUnit.ROTATIONS)
 CTRlimite = 0
+Calculateur = 0
+compteur = 0
+RATIO = 0
 rotation = 0
 NbPhoto = 0
+motor: stepperMotor.Motor = None
 motor = stepperMotor.create_motor(DigitalPin.P13,
     DigitalPin.P14,
     DigitalPin.P15,
     DigitalPin.P16)
+pins.digital_write_pin(DigitalPin.P1, 0)
 pins.digital_write_pin(DigitalPin.P8, 0)
 NbPhoto = 0
 rotation = 1
+RATIO = 15
 
 def on_forever():
-    basic.show_string("" + str((compteur)))
-    basic.pause(1000)
-    basic.clear_screen()
-basic.forever(on_forever)
-
-def on_forever2():
     global NbPhoto, compteur, CTRlimite
     if NbPhoto < 1:
         NbPhoto = 0
         compteur = 0
         CTRlimite = 0
+basic.forever(on_forever)
+
+def on_forever2():
+    global CTRlimite
+    if CTRlimite > RATIO:
+        winner()
+        CTRlimite = 0
 basic.forever(on_forever2)
+
+def on_forever3():
+    basic.show_string("" + str(compteur))
+    basic.pause(1000)
+    basic.clear_screen()
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-function _1_degret () {
-    motor.setDelay(10)
-    motor.moveClockwise(Calculateur, stepUnit.Rotations)
-}
 input.onButtonPressed(Button.A, function () {
     basic.pause(100)
     NbPhoto += 1
@@ -54,9 +50,13 @@ function PriseDeVue () {
     pins.digitalWritePin(DigitalPin.P8, 0)
     basic.pause(1000)
 }
+function _1_degret () {
+    motor.setDelay(10)
+    motor.moveClockwise(Calculateur, stepUnit.Rotations)
+}
 let CTRlimite = 0
-let compteur = 0
 let Calculateur = 0
+let compteur = 0
 let RATIO = 0
 let rotation = 0
 let NbPhoto = 0
@@ -67,10 +67,18 @@ DigitalPin.P14,
 DigitalPin.P15,
 DigitalPin.P16
 )
+pins.digitalWritePin(DigitalPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P8, 0)
 NbPhoto = 0
 rotation = 1
 RATIO = 15
+basic.forever(function () {
+    if (NbPhoto < 1) {
+        NbPhoto = 0
+        compteur = 0
+        CTRlimite = 0
+    }
+})
 basic.forever(function () {
     if (CTRlimite > RATIO) {
         winner()
@@ -78,14 +86,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    basic.showString("" + (compteur))
+    basic.showString("" + compteur)
     basic.pause(1000)
     basic.clearScreen()
-})
-basic.forever(function () {
-    if (NbPhoto < 1) {
-        NbPhoto = 0
-        compteur = 0
-        CTRlimite = 0
-    }
 })
