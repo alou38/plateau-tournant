2ffb468eb18cc8d85ce319dc5966ee44c5a41af3
alou38 <72703710+alou38@users.noreply.github.com>
2021-03-29 15:32:24 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    TIMER = 5000
     basic.pause(100)
     NbPhoto += 1
     basic.pause(100)
@@ -20,12 +21,49 @@ function winner () {
     basic.clearScreen()
 }
 input.onButtonPressed(Button.B, function () {
+    TIMER = 5000
     basic.pause(100)
     NbPhoto += -1
     basic.pause(100)
     compteur = NbPhoto
 })
-input.onPinPressed(TouchPin.P1, function () {
+function PriseDeVue () {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    basic.pause(1000)
+}
+function _1_degret () {
+    motor.setDelay(10)
+    motor.moveClockwise(Calculateur, stepUnit.Rotations)
+}
+let CTRlimite = 0
+let Calculateur = 0
+let compteur = 0
+let TIMER = 0
+let NbPhoto = 0
+let motor: stepperMotor.Motor = null
+motor = stepperMotor.createMotor(
+DigitalPin.P13,
+DigitalPin.P14,
+DigitalPin.P15,
+DigitalPin.P16
+)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P8, 0)
+NbPhoto = 0
+let rotation = 1
+let RATIO = 15
+TIMER = 50000
+basic.forever(function () {
+    if (NbPhoto < 1) {
+        NbPhoto = 0
+        compteur = 0
+        CTRlimite = 0
+    }
+})
+basic.forever(function () {
+    basic.pause(TIMER)
     // calcule
     Calculateur = RATIO / NbPhoto
     CTRlimite = Calculateur
@@ -44,49 +82,14 @@ input.onPinPressed(TouchPin.P1, function () {
     }
     CTRlimite = 0
 })
-function PriseDeVue () {
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    basic.pause(1000)
-}
-function _1_degret () {
-    motor.setDelay(10)
-    motor.moveClockwise(Calculateur, stepUnit.Rotations)
-}
-let CTRlimite = 0
-let Calculateur = 0
-let compteur = 0
-let RATIO = 0
-let rotation = 0
-let NbPhoto = 0
-let motor: stepperMotor.Motor = null
-motor = stepperMotor.createMotor(
-DigitalPin.P13,
-DigitalPin.P14,
-DigitalPin.P15,
-DigitalPin.P16
-)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P8, 0)
-NbPhoto = 0
-rotation = 1
-RATIO = 15
 basic.forever(function () {
-    if (NbPhoto < 1) {
-        NbPhoto = 0
-        compteur = 0
-        CTRlimite = 0
-    }
+    basic.showString("" + (compteur))
+    basic.pause(1000)
+    basic.clearScreen()
 })
 basic.forever(function () {
     if (CTRlimite > RATIO) {
         winner()
         CTRlimite = 0
     }
-})
-basic.forever(function () {
-    basic.showString("" + compteur)
-    basic.pause(1000)
-    basic.clearScreen()
 })
